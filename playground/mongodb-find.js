9317d678b1fdbb2a63893955ccd107e81e7a5731
tonyOP1985 <toliverpaull@gmail.com>
2017-08-14 17:47:48 -0400

    init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,35 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server.');
+  }
+  console.log('Connected to MongoDB server.');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5991e4897e0bb50cd4bf8517')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', error);
+  // });
+
+  db.collection('Users').find({name: 'Juvia Lockser'}).toArray().then((docs) => {
+    console.log('Users');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to getch Users');
+  });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', error);
+  // });
+
+  // db.close();
+});
