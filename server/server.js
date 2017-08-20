7a647054cc831d17218994f931004db11850cfd6
tonyOP1985 <toliverpaull@gmail.com>
2017-08-20 17:00:59 -0400

    Test the DELETE /todo/:id route

--- server/server.js
+++ server/server.js
@@ -48,8 +48,32 @@ app.get('/todo/:id', (req, res) => {
     }
 
     res.send({todo});
-  }).catch((e) => res.status(400).send());
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
+
+
+//delete todos
+app.delete('/todo/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+
 
 app.listen(port, () => {
   console.log(`Started up on port ${port}`);
