645f24c43c15029fa50e84c36fb603a962ff6e99
tonyOP1985 <toliverpaull@gmail.com>
2017-08-20 21:26:35 -0400

    config for mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost:27017/TodoApp', {useMongoClient: true});
 // mongoose.connect('mongodb://admin:admin@ds151163.mlab.com:51163/node-api-db', {useMongoClient: true});
-mongoose.connect((process.env.MONGODB_URI, {useMongoClient: true}) || ('mongodb://admin:admin@ds151163.mlab.com:51163/node-api-db', {useMongoClient: true}) );
+// mongoose.connect((process.env.MONGODB_URI, {useMongoClient: true}) || ('mongodb://admin:admin@ds151163.mlab.com:51163/node-api-db', {useMongoClient: true}) );
+mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
 
 
 module.exports = {mongoose};
