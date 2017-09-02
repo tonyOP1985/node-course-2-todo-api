8b6773ad52c0118df71973d3845dd98e45a2955d
tonyOP1985 <toliverpaull@gmail.com>
2017-09-02 12:49:37 -0400

    re-set up app for heroku

--- server/config/config.js
+++ server/config/config.js
@@ -8,3 +8,6 @@ else if (env === 'test') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
 }
+
+
+// MONGODB_URI: mongodb://heroku_6g8qdkh0:a82u63krd3im2srcsvu921dslc@ds123124.mlab.com:23124/heroku_6g8qdkh0
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://admin:admin@ds151163.mlab.com:51163/node-api-db', {useMongoClient: true});
-mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
+mongoose.connect((process.env.MONGODB_URI, {useMongoClient: true}) || ('mongodb://localhost:27017/TodoApp', {useMongoClient: true}));
+// mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
 
 
 
