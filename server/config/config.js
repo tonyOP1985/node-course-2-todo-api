0f0158b50e057b8c596e8dce06b0cfdfe27a6e29
tonyOP1985 <toliverpaull@gmail.com>
2017-09-02 13:38:44 -0400

    configered app

--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,16 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+if (env == "development" || env == "test") {
+  var config = require("./config.json");
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
 }
-else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-}
+
+
+
 
 
 // MONGODB_URI: mongodb://heroku_6g8qdkh0:a82u63krd3im2srcsvu921dslc@ds123124.mlab.com:23124/heroku_6g8qdkh0
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect((process.env.MONGODB_URI, {useMongoClient: true}) || ('mongodb://localhost:27017/TodoApp', {useMongoClient: true}));
-// mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
+// mongoose.connect((process.env.MONGODB_URI, {useMongoClient: true}) || ('mongodb://localhost:27017/TodoApp', {useMongoClient: true}));
+mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
 
 
 
